85c8036378a33e16cb5ea4dd13155511a27acfde
unknown <luizc04@hotmail.com>
2022-03-20 06:57:31 -0300

    Projeto Enviado

---
+++ pizzas.js
@@ -0,0 +1,18 @@
+let pizzaJson = [
+    {id:1, name:'Pão de Centeio', img:'images/produto01.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:2, name:'Pão de Grãos', img:'images/produto02.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:3, name:'Pão de Nozes', img:'images/produto21.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:4, name:'Frutas e Nata ', img:'images/produto04.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:5, name:'Maçã', img:'images/produto05.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:6, name:'Cream Cheese', img:'images/produto06.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:7, name:'Chocolate', img:'images/produto20.png', price:10.31, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:8, name:'Pão Italiano', img:'images/produto12.png', price:21.18, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:9, name:'Pão de Abóbora', img:'images/produto11.png', price:12.34, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+
+    {id:10, name:'Pão de Sementes', img:'images/produto13.png', price:18.26, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:11, name:'Cenoura', img:'images/produto14.png', price:10.41, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:12, name:'Sourdough ', img:'images/produto15.png', price:21.00, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:13, name:'Coco', img:'images/produto17.png', price:16.38, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:14, name:'Red Velvet', img:'images/produto16.png', price:17.50, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'},
+    {id:15, name:'Pão Alentejano', img:'images/produto18.png', price:14.20, sizes:['320g', '530g', '860g'], description:'Idéia de receita e fotografia de comida de pão caseiro e bolo interessante'}
+];
